9e71be9760ce45cc78309189b70692a8ab6c2954
willshang-study <751989896@qq.com>
2021-09-28 08:14:30 +0800

    java8-parallel流的学习

--- src/mian/java/com/jdk8/newfeatures/chapter04/ParallelStreamError.java
+++ src/mian/java/com/jdk8/newfeatures/chapter04/ParallelStreamError.java
@@ -11,40 +11,31 @@ public class ParallelStreamError {
 
         // 陷阱1：流不易迭代划分为多个子流
         // 1. 我们很难把iterate分成多个独立块来并行执行。(每次生成一个新的结果必须依赖上一次的结果)
-        OptionalLong timeIterateError = LongStream.range(0,10).map(item->{
-            long startTime = System.nanoTime();
-            IntStream.iterate(0,a->++a).limit(10000000).parallel().sum();
-            long period = System.nanoTime() - startTime;
+        OptionalLong timeIterateError = LongStream.range(0,3).map(item->{
+            long startTime = System.currentTimeMillis();
+            IntStream.iterate(0,a->++a).limit(10000000).parallel().reduce(Integer::sum);
+            long period = System.currentTimeMillis() - startTime;
             return period;
         }).min();
 
-        // 2. Stream.iterate生成的是装箱的对象，必须拆箱成数字才能求和
-        OptionalLong timeIterateBoxed = LongStream.range(0,10).map(item->{
-            long startTime = System.nanoTime();
-            Stream.iterate(0, a->++a).limit(10000000).parallel().reduce(Integer::sum);
-            long period = System.nanoTime() - startTime;
-            return period;
-        }).min();
-
-        OptionalLong timeRange = LongStream.range(0,10).map(item->{
-            long startTime = System.nanoTime();
+        OptionalLong timeRange = LongStream.range(0,3).map(item->{
+            long startTime = System.currentTimeMillis();
             IntStream.rangeClosed(0,10000000).sum();
-            long period = System.nanoTime() - startTime;
+            long period = System.currentTimeMillis() - startTime;
             return period;
         }).min();
         System.out.println("iterate(陷阱：流不易分割)："+timeIterateError.getAsLong());
-        System.out.println("iterate(陷阱：需要拆箱成原始类型计算求和)："+timeIterateBoxed.getAsLong());
-        System.out.println("range："+timeRange.getAsLong());
-
-        // 陷阱2：算法改变了共享状态
-        Accumulator accumulator = new Accumulator();
-        // 每一次迭代改变 accumulator 对象的 sum 变量
-        LongStream.range(0,10000).forEach(accumulator::add);
-        System.out.println("串行流::"+accumulator.sum);
-        Accumulator accumulator2 = new Accumulator();
-        // ForkJoinPool线程池多个线程竞争sum，且未加锁,此处将sum声明为volatile也无济于事
-        LongStream.range(0,10000).parallel().forEach(accumulator2::add);
-        System.out.println("并行流::"+accumulator2.sum);
+        System.out.println("range                   ："+timeRange.getAsLong());
+//
+//        // 陷阱2：算法改变了共享状态
+//        Accumulator accumulator = new Accumulator();
+//        // 每一次迭代改变 accumulator 对象的 sum 变量
+//        LongStream.range(0,10000).forEach(accumulator::add);
+//        System.out.println("串行流::"+accumulator.sum);
+//        Accumulator accumulator2 = new Accumulator();
+//        // ForkJoinPool线程池多个线程竞争sum，且未加锁,此处将sum声明为volatile也无济于事
+//        LongStream.range(0,10000).parallel().forEach(accumulator2::add);
+//        System.out.println("并行流::"+accumulator2.sum);
     }
 
     public static class Accumulator{
